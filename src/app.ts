5b5168f65992e91519848d8f8463d6fd1f3525a3
Marinho <jmarinhofn@gmail.com>
2024-08-23 14:37:57 -0300

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,10 @@
 import { createServer } from './config/server';
 import userRoutes from './routes/userRoutes';
 import authRoutes from './routes/authRoutes';
-// import medicoRoutes from './routes/medicoRoutes';
-// import consultaRoutes from './routes/consultaRoutes';
 
 const app = createServer();
 
 app.use('/api', userRoutes);
 app.use('/api', authRoutes);
-// app.use('/api', medicoRoutes);
-// app.use('/api', consultaRoutes);
 
 export default app;

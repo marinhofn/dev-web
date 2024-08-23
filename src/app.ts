6c4edeb33ce1f1e40b3e63b42e78cdf806a95905
Marinho <jmarinhofn@gmail.com>
2024-08-23 14:39:42 -0300

    update routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import { createServer } from './config/server';
 import userRoutes from './routes/userRoutes';
 import authRoutes from './routes/authRoutes';
+import estacionamentoRoutes from './routes/estacionamentoRoutes';
+import reservaRoutes from './routes/reservaRoutes';
+import vagaRoutes from './routes/vagaRoutes';
+import veiculoRoutes from './routes/veiculoRoutes';
 
 const app = createServer();
 
 app.use('/api', userRoutes);
 app.use('/api', authRoutes);
+app.use('/api', estacionamentoRoutes);
+app.use('/api', reservaRoutes);
+app.use('/api', vagaRoutes);
+app.use('/api', veiculoRoutes);
+
 
 export default app;
